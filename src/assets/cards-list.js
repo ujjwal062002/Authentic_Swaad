9c136add210257d998b9164178c10fda9f23d9cf
Ujjwal Narayan <narayanujjwal1@gmail.com>
2023-11-13 17:03:54 +0530

    first commit

---
+++ src/assets/cards-list.js
@@ -0,0 +1,153 @@
+export const list2 = [
+  {
+    rating: "4",
+    desc: "Beach and Sunset Views",
+    imgSrc: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
+      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
+      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 0000",
+  },
+  {
+    rating: "4",
+    desc: "Beach and Sunset Views",
+    imgSrc: [
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+    ],
+    price: "30000",
+    date: "25-20 May",
+    title: "Hotel 3",
+  },
+  {
+    rating: "4",
+    desc: "Beach and Sunset Views",
+    imgSrc: [
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+    ],
+    price: "45000",
+    date: "10-20 Oct",
+    title: "Hotel 3",
+  },
+  {
+    rating: "4",
+    desc: "Beach and Sunset Views",
+    imgSrc: [
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
+      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
+      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+];
+
+export const list = [
+  {
+    rating: "4.2",
+    desc: "Mrs. Reema Kumari",
+    imgSrc: [
+      "https://cookwithrenu.com/wp-content/uploads/2020/09/Rajasthani-Dal-Baati-Churma_Featured-Image.jpg",
+      "https://www.archanaskitchen.com/images/archanaskitchen/0-Archanas-Kitchen-Recipes/2018/Rajasthani_Dal_Bati_Churma_Recipe-5.jpg",
+      "https://media.istockphoto.com/id/1491970790/video/happy-indian-old-lady-showing-victory-symbol.jpg?s=640x640&k=20&c=u_slohL31-d9mU6Q5IDzUqI8BsN-bfxvmZOVfc5biSc=",
+      // "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+    ],
+    price: "150",
+    date: "Malviya Nagar",
+    title: "Dal Baati Churma",
+  },
+  {
+    rating: "4.3",
+    desc: "Mrs. Lata Kumari",
+    imgSrc: [
+      "https://www.deliciousmagazine.co.uk/wp-content/uploads/2021/01/Chicken_Curry-768.jpg",
+      "https://yummyindiankitchen.com/wp-content/uploads/2020/10/punjabi-chicken-curry-gravy-recipe.jpg",
+      "https://t3.ftcdn.net/jpg/04/34/56/86/360_F_434568683_CBQ5OC5KuDe59qXgM5LCamhDgiFwgRuj.jpg",
+    ],
+    price: "180",
+    date: "Dwarka Sector 10",
+    title: "Punjabi Chicken Curry",
+  },
+  {
+    rating: "4.0",
+    desc: "Mrs. Seema Reddy",
+    imgSrc: [
+      "https://static.toiimg.com/thumb/69038542.cms?imgsize=563452&width=800&height=800",
+      "https://images.aws.nestle.recipes/resized/11e8846b57d5fa6c8c15ae84edff598e_A42I8734_944_531.jpg",
+      "https://i.pinimg.com/1200x/7a/84/da/7a84dad4d5b01091373ae50d1658e84b.jpg",
+    ],
+    price: "200",
+    date: "Dwarka Mor",
+    title: "Hyderabadi Biryani",
+  },
+  {
+    rating: "4.4",
+    desc: "Mrs. Kiran Seth",
+    imgSrc: [
+      "https://myfoodstory.com/wp-content/uploads/2022/01/Misal-Pav-1.jpg",
+      "https://pbs.twimg.com/media/EWgXaP4XgAI9WQv.jpg:large",
+      "https://static.langimg.com/photo/imgsize-1445066,msid-79424290/maharashtra-times.jpg",
+      // "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+    ],
+    price: "80",
+    date: "Rajouri Garden",
+    title: "Misal Pav",
+  },
+  {
+    rating: "4.5",
+    desc: "Mr. Krishna Reddy",
+    imgSrc: [
+      "https://www.onmycanvas.com/wp-content/uploads/2021/06/triangular-masala-dosa-feature.jpeg",
+      "https://thumbs.dreamstime.com/b/portrait-cheerful-indian-senior-old-man-pointing-presenting-hands-folded-pose-retired-wears-white-kurta-195341852.jpg",
+    ],
+    price: "60",
+    date: "Lodhi Garden",
+    title: "Masala Dosa",
+  },
+  {
+    rating: "4.4",
+    desc: "Mr. Roshan Singh",
+    imgSrc: [
+      "https://static.toiimg.com/thumb/75489936.cms?imgsize=2071135&width=800&height=800",
+      "https://photos.mixcommerce.co/wp-content/uploads/indian-uncle-smiling-with-a-specs-scaled-png.png",
+      // "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
+      // "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+    ],
+    price: "250",
+    date: "Malviya Nagar",
+    title: "Kosha Mangsho",
+  },
+  {
+    rating: "4.5",
+    desc: "Mrs. Surbhi Jain",
+    imgSrc: [
+      "https://cdn2.foodviva.com/static-content/food-images/snacks-recipes/khaman-dhokla-recipe/khaman-dhokla-recipe.jpg",
+      "https://www.indianveggiedelight.com/wp-content/uploads/2023/02/khaman-dhokla-featured.jpg",
+      "https://i.pinimg.com/736x/b9/6e/08/b96e08a7c4aab3a5f7f2b53f28261db5.jpg",
+      // "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+    ],
+    price: "120",
+    date: "Dwarka",
+    title: "Dhokla",
+  },
+  {
+    rating: "4.3",
+    desc: "Mrs. Asha Kumari",
+    imgSrc: [
+      "https://www.whiskaffair.com/wp-content/uploads/2021/05/Goan-Fish-Curry-2-3.jpg",
+      "https://satyamskitchen.com/wp-content/uploads/2021/05/website.jpg",
+      "https://pbs.twimg.com/media/EXIt2v9U8AAHjv2.jpg:large",
+      // "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+    ],
+    price: "200",
+    date: "Dwarka Sector 10",
+    title: "Fish Curry",
+  },
+];
---
+++ src/components/Header/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import logo from "../../assets/logo/long-logo.png";
+import "./styles.css";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import LanguageIcon from "@mui/icons-material/Language";
+import BasicMenu from "./ProfileMenu";
+import SimpleBottomNavigation from "./BottomNav";
+import MobileSearchBar from "../MobileSearchBar";
+
+function Header() {
+  return (
+    <div className="navbar">
+      <img
+  src={logo}
+  alt="logo"
+  className="navbar-logo"
+  style={{ width: '150px', height: 'auto' }}
+/>
+      <div className="search-bar">
+        {/* <div className="search-bar-text">Anywhere</div>
+        <div className="search-bar-text">Any Week</div> */}
+        <div className="search-bar-text2">Seach Food Items Here...</div>
+        <div className="search-icon-div">
+          <SearchRoundedIcon className="search-icon" />
+        </div>
+      </div>
+      <div className="profile-container">
+        <div className="airbnb-your-home">Your Home Cooked Meal</div>
+        <div className="airbnb-your-home">
+          <LanguageIcon sx={{ fontSize: "1.3rem" }} />
+        </div>
+        <div className="profile-div">
+          <BasicMenu />
+        </div>
+      </div>
+      <MobileSearchBar />
+      <SimpleBottomNavigation />
+    </div>
+  );
+}
+
+export default Header;
